9af4f77ef3ca3807b53df281326c0d15823b47aa
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2025-02-09 17:41:49 +0000

    was gonna do fashion
    didn't have lyrics
    
    so famous on the internet instead, I guess that's more relevant
    
    I was losing my mind, crying all night over some likes
    Maybe I'll start selling some fake jewelry online
    Living in a sponsored ad, I feel personally attacked
    Oh, by the way, how'd you get your account verified?
    
    What's trending lately?
    Fake news, Drake memes
    Fit frames, blue jeans
    Follow back, please
    
    She wants to be famous on the internet
    His friends are all culturally relevant
    Gets high on likes and Percocets
    Hashtag not my prеsident
    DM for Chanel e-cigarеttes
    AR-15 for self-defense
    I'm sleeping in the car 'cause I'm too depressed
    She wants to be famous on the internet
    
    If you want to feel good and lose a few pounds, just hit subscribe
    I followed 'cause lately I've felt ugly inside
    'There's no such thing as climate change, don't worry 'bout the hurricanes
    Oh, by the way, how'd you get your account monetised?
    
    Food porn, slay me
    Starbucks, baby
    Social disease
    Someone kill me
    
    She wants to be famous on the internet
    His friends are all culturally relevant
    Gets high on likes and Percocets
    Hashtag not my president
    DM for Chanel e-cigarettes
    AR-15 for self-defense
    I'm sleeping in the car 'cause I'm too depressed
    She wants to be famous on the internet
    
    She wants to be famous
    She wants to be famous
    I wish I were famous
    
    Who hacked my account? Call the police
    Slipping through the cracks in my screen
    Falling in love with a machine
    Wanna be liked desperately
    
    She wants to be famous on the internet
    His friends are all culturally relevant
    Gets high on likes and Percocets
    Hashtag not my president
    DM for Chanel e-cigarettes
    AR-15 for self-defense
    I'm sleeping in the car 'cause I'm too depressed
    She wants to be famous on the internet
    
    She wants to be famous
    She wants to be famous
    I wish I were famous, yeah
    I wish I were famous
    I wish I were famous
    I wish I were famous

--- js/gallery.js
+++ js/gallery.js
@@ -10,20 +10,66 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  const galleryImages = document.querySelectorAll('.gallery img');
   const previewWindow = document.getElementById('preview-window');
   const previewImage = document.getElementById('preview-image');
   const previewTitle = document.getElementById('preview-title');
-  const previewCloseButton = document.getElementById('preview-close-button');
 
-  galleryImages.forEach(function (img) {
-    img.addEventListener('click', function () {
-      previewImage.src = img.src;
-      previewImage.alt = img.alt;
-      previewTitle.textContent = img.dataset.timestamp;
-      previewWindow.style.display = 'block';
-    });
+  fetch('https://assets.vivwebz.net/vrchat_files.json')
+    .then(response => response.json())
+    .then(songs => {
+      const picturesArray = Object.values(songs);
+      for (const picture of picturesArray) {
+        let timestamp = picture.split("t_")[1]; // fuck its one of these again
+        timestamp = timestamp.split(".")[0]; // remove the extension
+        timestamp = timestamp.replaceAll("_", " "); // replace the _ with a space
+        let timestampStart = timestamp.split(" ")[0];
+        let timestampEnd = timestamp.split(" ")[1];
+        timestampEnd = timestampEnd.replaceAll("-", ":");
+        timestamp = timestampStart + " " + timestampEnd;
+        document.getElementById("vrc-pictures").innerHTML += `<img src="https://assets.vivwebz.net/${picture}" alt="${timestamp}" data-timestamp="${timestamp}">`;
+      }
+    })
+    .then(() => {
+      const galleryImages = document.querySelectorAll('.gallery img');
+      galleryImages.forEach(image => {
+        image.addEventListener('click', function () {
+          previewImage.src = this.src;
+          previewTitle.textContent = this.alt;
+          previewWindow.style.display = 'block';
+        });
+      });
+    })
+    .catch(error => console.error('Error with image listing:', error));
+
+  const npc = document.getElementById('npc');
+  const npcTooltip = document.getElementById('npc-tooltip');
+  const dialogues = [
+    "I'm just your regular, every day, digital dragon.",
+    "Vivian put me in charge of her VRChat pictures, I get paid XMR to be here.",
+    "I am a sentient, living, artificial-intelligence, stuck inside this website.",
+    "VRChat is pretty cool, huh?",
+    "My shift ends at 6AM, and starts at 6AM. 24/7 grinding out here."
+  ];
+  let isDialogueActive = false;
+  npc.addEventListener('click', function () {
+    if (isDialogueActive) return;
+    isDialogueActive = true;
+    const randomDialogue = dialogues[Math.floor(Math.random() * dialogues.length)];
+    npcTooltip.textContent = randomDialogue;
+    npcTooltip.classList.remove('tooltip-fade-out');
+    npcTooltip.style.display = 'block';
+    npcTooltip.classList.add('tooltip-fade-in');
+    setTimeout(function () {
+      npcTooltip.classList.remove('tooltip-fade-in');
+      npcTooltip.classList.add('tooltip-fade-out');
+      setTimeout(function () {
+        npcTooltip.style.display = 'none';
+        npcTooltip.classList.remove('tooltip-fade-out');
+        isDialogueActive = false;
+      }, 500);
+    }, 3000);
   });
+  const previewCloseButton = document.getElementById('preview-close-button');
 
   previewCloseButton.addEventListener('click', function () {
     previewWindow.style.display = 'none';
